5059a9e658bd0dea1b786054647b85cbe4faed2b
unsighera <eddy.weston@bk.ru>
2024-04-05 18:36:10 +0300

    Добавьте файлы проекта.

---
+++ Calc ToDo Api/Data/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Calc_ToDo_Api.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
